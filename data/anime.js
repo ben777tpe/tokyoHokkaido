dce7f50fd9d07adb139fcb4a0b6a4ee98e5cdf96
ben777tpe <ben777@gmail.com>
2026-08-20 07:42:49 +0800

    Add files via upload

---
+++ data/anime.js
@@ -0,0 +1,126 @@
+// data/anime.js - Conan Plaza, Claw Machines, Mega Stationery, and 100-Yen Shops
+window.TRIP_ANIME = {
+  "conan": {
+    "title": "🕵️‍♂️ 名偵探柯南 2026 年度旗艦快閃：Detective Conan Plaza",
+    "location": "東京巨蛋城 Central Park (東京都文京区後楽1-3-61)",
+    "dates": "2026年4月1日 ～ 9月27日 (8/27 週四在展期內！)",
+    "hours": "11:00～19:00",
+    "holiday": "展期內每日無休 ｜ 8/27(四) 保證正常營運！",
+    "holidayBadge": "text-emerald-700 font-bold bg-emerald-50 px-2 py-0.5 rounded border border-emerald-200",
+    "transit": "高田馬場搭 JR 山手線至新宿轉 JR 總武線 12 分鐘直達「水道橋站」西口步行 3 分鐘",
+    "googleUrl": "https://www.google.com/maps/search/?api=1&query=Tokyo+Dome+City+Central+Park",
+    "appleUrl": "https://maps.apple.com/?q=Tokyo+Dome+City+Central+Park",
+    "history": "配合 2026 柯南最新劇場版《名偵探柯南：高速公路的墮天使》之全國最大巡迴旗艦展區，展示等身大角色立牌與名場面打卡牆。",
+    "highlights": [
+      "2026 劇場版限定全套角色壓克力立牌、名言金屬徽章、限定帆布袋",
+      "柯南專屬角色限定拍貼機（全家拍柯南框大頭照紀念！）",
+      "滿額限定贈送：2026 劇場版雷射角色透卡特典",
+      "會場限定柯南聯名手帳、原子筆與收納文具"
+    ],
+    "officialUrl": "https://twitter.com/conan_plaza"
+  },
+  "claw_machines": [
+    {
+      "name": "高田馬場 BIGBOX 6F タイトーステーション (Taito Station)",
+      "city": "東京・高田馬場",
+      "location": "新宿区高田馬場1-35-3 BIGBOX 6F (飯店正對面過馬路即達)",
+      "hours": "10:00～24:00 (深夜營業)",
+      "holiday": "無休 (年中無休)",
+      "scale": "高田馬場最大！百台最新夾娃娃機",
+      "desc": "就在飯店正對面！下樓過個斑馬線就能玩，營業到午夜 24:00，寶可夢、吉伊卡哇、排球少年等最新日本景品應有盡有，抓完直接抱回飯店房間！",
+      "googleUrl": "https://www.google.com/maps/search/?api=1&query=BIGBOX+高田馬場",
+      "appleUrl": "https://maps.apple.com/?q=BIGBOX+高田馬場"
+    },
+    {
+      "name": "池袋 萬代南夢宮 cross store 3F (Bandai Namco)",
+      "city": "東京・池袋",
+      "location": "豊島区東池袋3-1-2 Sunshine City World Import Mart 3F",
+      "hours": "10:00～21:00",
+      "holiday": "無休 (年中無休)",
+      "scale": "世界級動漫娛樂城！3,000 台扭蛋機 ＋ 巨型夾娃娃專區",
+      "desc": "池袋太陽城 3F，擁有超過 3,000 台扭蛋機的驚人扭蛋百貨與巨型萬代夾娃娃機，各種動漫聯名模型景品最齊全！",
+      "googleUrl": "https://www.google.com/maps/search/?api=1&query=バンダイナムコ+Cross+Store+東京",
+      "appleUrl": "https://maps.apple.com/?q=バンダイナムコ+Cross+Store+東京"
+    },
+    {
+      "name": "札幌 GiGO すすきの (GiGO Susukino)",
+      "city": "北海道・札幌薄野",
+      "location": "札幌市中央区南4条西4丁目1-1 (薄野站旁，飯店步行3分)",
+      "hours": "10:00～23:00",
+      "holiday": "無休 (年中無休)",
+      "scale": "全新整棟！120 台最新夾娃娃機",
+      "desc": "薄野站旁全新落成整棟 GiGO 遊戲中心，120 台夾娃娃機專區，北海道限定景品與大型玩偶極多，吃養成吉思汗烤肉後順遊！",
+      "googleUrl": "https://www.google.com/maps/search/?api=1&query=GiGO+すすきの",
+      "appleUrl": "https://maps.apple.com/?q=GiGO+すすきの"
+    },
+    {
+      "name": "札幌 タイトーステーション 狸小路4丁目店",
+      "city": "北海道・札幌狸小路",
+      "location": "札幌市中央区南3条西4丁目 (狸小路 4 丁目內)",
+      "hours": "10:00～23:30",
+      "holiday": "無休 (年中無休)",
+      "scale": "狸小路 4 樓層大型夾娃娃旗艦",
+      "desc": "位於狸小路 4 丁目全天候拱廊內，4 層樓滿滿夾娃娃機與拍貼機，逛街採買伴手禮時隨時可以進去夾一隻！",
+      "googleUrl": "https://www.google.com/maps/search/?api=1&query=タイトーステーション+札幌狸小路4丁目店",
+      "appleUrl": "https://maps.apple.com/?q=タイトーステーション+札幌狸小路4丁目店"
+    },
+    {
+      "name": "東京巨蛋城 GiGO (GiGO Tokyo Dome City)",
+      "city": "東京・後樂園",
+      "location": "東京都文京区後楽1-3-61 (Yellow Building 2F)",
+      "hours": "10:00～22:00",
+      "holiday": "無休 (年中無休)",
+      "scale": "巨蛋主題夾娃娃館",
+      "desc": "東京巨蛋城遊樂區旁，逛完柯南快閃店與吃完晚餐後，順道帶孩子來夾最新動漫公仔與玩偶！",
+      "googleUrl": "https://www.google.com/maps/search/?api=1&query=GiGO+東京ドームシティ",
+      "appleUrl": "https://maps.apple.com/?q=GiGO+東京ドームシティ"
+    }
+  ],
+  "stationery_and_100yen": [
+    {
+      "name": "大丸藤井セントラル (Daimaru Fujii Central)",
+      "type": "百年文具總殿堂",
+      "location": "札幌市中央区南1条西3丁目2 (大通公園南側步行2分)",
+      "hours": "10:00～19:00",
+      "desc": "創立於 1892 年，整整 7 層樓的文具百貨！1F 鋼筆與手帳、2F 日本和紙與美術、3F 事務文具、4F 精品紙膠帶與卡片，文具控必朝聖！",
+      "googleUrl": "https://www.google.com/maps/search/?api=1&query=大丸藤井セントラル+札幌",
+      "appleUrl": "https://maps.apple.com/?q=大丸藤井セントラル+札幌"
+    },
+    {
+      "name": "澀谷 Loft (Shibuya Loft)",
+      "type": "旗艦文具設計百貨",
+      "location": "東京都渋谷区宇田川町21-1 (澀谷站步行5分)",
+      "hours": "11:00～21:00",
+      "desc": "整棟 6 層樓，B1F「文房具世界」擁有全東京最齊全的日本設計筆款、MIDORI 手帳、mt 紙膠帶、貼紙與創意辦公文具！",
+      "googleUrl": "https://www.google.com/maps/search/?api=1&query=渋谷ロフト",
+      "appleUrl": "https://maps.apple.com/?q=渋谷ロフト"
+    },
+    {
+      "name": "芳林堂書店 高田馬場店 (BIGBOX 4F)",
+      "type": "站前大型書店與文具館",
+      "location": "新宿区高田馬場1-35-3 BIGBOX 4F (飯店正對面)",
+      "hours": "10:00～21:00",
+      "desc": "高田馬場最大綜合書店與文具館，擁有豐富的文具專區、手帳、動漫設定集與日本漫畫！",
+      "googleUrl": "https://www.google.com/maps/search/?api=1&query=芳林堂書店+高田馬場店",
+      "appleUrl": "https://maps.apple.com/?q=芳林堂書店+高田馬場店"
+    },
+    {
+      "name": "Seria (セリア) 高田馬場店 ＆ 池袋太陽城店",
+      "type": "日本最高人氣 100 円設計質感日雜",
+      "location": "高田馬場店：Peacock Store B1F ｜ 池袋店：Sunshine City ALTA 1F",
+      "hours": "10:00～20:00 / 21:00",
+      "desc": "全品項均一價 100 円（含稅 110 円）！日系鄉村風、手帳貼紙、收納盒、動漫小卡收納保護套超高 CP 值！",
+      "googleUrl": "https://www.google.com/maps/search/?api=1&query=Seria+高田馬場",
+      "appleUrl": "https://maps.apple.com/?q=Seria+高田馬場"
+    },
+    {
+      "name": "Standard Products (moyuk SAPPORO 2F)",
+      "type": "大創旗下無印風 300 円質感生活旗艦",
+      "location": "札幌市中央区南2条西3丁目20 moyuk SAPPORO 2F",
+      "hours": "10:00～20:00",
+      "desc": "主打極簡無印風、天然木質、日本職人工藝餐具與香氛，均一價 300 円起，質感極佳！",
+      "googleUrl": "https://www.google.com/maps/search/?api=1&query=Standard+Products+moyuk+SAPPORO",
+      "appleUrl": "https://maps.apple.com/?q=Standard+Products+moyuk+SAPPORO"
+    }
+  ]
+};
---
+++ data/checklist.js
@@ -0,0 +1,69 @@
+// data/checklist.js - 8/25 NIPPON Car Rental Voucher & Interactive Checklist
+window.TRIP_CHECKLIST = {
+  "rental": {
+    "company": "NIPPON Rent-A-Car 札幌站前營業所",
+    "reservationNo": "101851488",
+    "password": "c60305a4",
+    "pickupTime": "2026/08/25 (二) 08:00",
+    "returnTime": "2026/08/25 (二) 20:00",
+    "pickupLocation": "北海道札幌市東区北7条東1丁目1番1号 (札幌站東口步行 5 分)",
+    "returnLocation": "同取車門市 (Map Code: 9 553 092)",
+    "phone": "011-741-0919",
+    "totalFee": "¥12,540 (現場信用卡付款)",
+    "included": "含消費稅、安心保險 (NOC 免除)、ETC 晶片卡租借、中文車載 GPS 導航",
+    "googleUrl": "https://www.google.com/maps/search/?api=1&query=北海道札幌市東区北7条東1丁目1番1号",
+    "appleUrl": "https://maps.apple.com/?q=北海道札幌市東区北7条東1丁目1番1号"
+  },
+  "items": [
+    {
+      "id": "c1",
+      "text": "護照 4 本（效期需在 6 個月以上）",
+      "cat": "證件"
+    },
+    {
+      "id": "c2",
+      "text": "台灣駕照正本 ＋ 日文駕照譯本（Ben 自駕必備！）",
+      "cat": "自駕"
+    },
+    {
+      "id": "c3",
+      "text": "日幣現金 ＋ 海外高回饋信用卡 2 張以上",
+      "cat": "財務"
+    },
+    {
+      "id": "c4",
+      "text": "Visit Japan Web (VJW) 填寫並截圖 QR Code",
+      "cat": "通關"
+    },
+    {
+      "id": "c5",
+      "text": "日本上網 eSIM / SIM 卡已開通並測試",
+      "cat": "通訊"
+    },
+    {
+      "id": "c6",
+      "text": "NIPPON 租車確認信截圖（預約號 101851488）",
+      "cat": "自駕"
+    },
+    {
+      "id": "c7",
+      "text": "手機防水袋、雨傘 / 輕便雨衣",
+      "cat": "隨身"
+    },
+    {
+      "id": "c8",
+      "text": "行動電源 ＋ 各型號充電線",
+      "cat": "隨身"
+    },
+    {
+      "id": "c9",
+      "text": "常備個人藥品（暈車藥、感冒胃腸藥、退熱貼）",
+      "cat": "健康"
+    },
+    {
+      "id": "c10",
+      "text": "折疊大行李袋 / 戰利品收納袋",
+      "cat": "購物"
+    }
+  ]
+};
